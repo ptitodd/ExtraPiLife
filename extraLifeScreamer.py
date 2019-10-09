000141e625b1119d5743a41145b71fcad3c5126e
Grant Gordon <grant.grdn@gmail.com>
2019-10-08 21:18:14 -0600

    Update team id

--- extraLifeScreamer.py
+++ extraLifeScreamer.py
@@ -7,14 +7,13 @@ import sys
 if __name__ == "__main__":
 
 	isDebug = "--debug" in sys.argv
-
 	handler = Screamer() if 'screamer' in sys.argv else DonationPrinter()
 
 	# using the teams endpoints instead of the donor endpoint because if we get
 	# over 100 donations then we have to deal with pagination. However, each
 	# donation will change the team object numDonations and sumDonations so the
 	# event will fire all the same.
-	service = DonationService("www.extra-life.org", "/api/teams/38776", handler)
+	service = DonationService("www.extra-life.org", "/api/teams/44435", handler)
 	if (isDebug):
 		service.startDebug()
 	else:
